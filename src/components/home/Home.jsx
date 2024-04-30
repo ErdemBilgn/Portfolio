1685efe1af9c500f016f860b5c696753038ba20a
Erdem <mr.erdembilgin@gmail.com>
2024-05-01 01:55:31 +0300

    Completed home component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import "./home.css";
+import Social from "./Social";
 import { useLanguage } from "../../contexts/LanguageContext";
+import Data from "./Data";
+import ScrollDown from "./ScrollDown";
 
 export default function Home() {
   const { language, changeLanguage, getTexts } = useLanguage();
   const texts = getTexts();
   return (
     <section className="home section" id="home">
-      <div className="home__container container grid"></div>
+      <div className="home__container container grid">
+        <div className="home__content grid">
+          <Social />
+
+          <div className="home__img"></div>
+
+          <Data />
+        </div>
+
+        <ScrollDown />
+      </div>
     </section>
   );
 }
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function Social() {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.instagram.com/erdembilgn/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-instagram"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/omererdembilgin"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-linkedin-alt"></i>
+      </a>
+      <a
+        href="https://github.com/ErdemBilgn"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+}
