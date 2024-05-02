dd0737de4131763cc78ed62104f6e3eb43a1886f
Erdem <mr.erdembilgin@gmail.com>
2024-05-02 04:40:20 +0300

    Added Active Link behaviour and scroll header background to Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,8 +4,16 @@ import { useLanguage } from "../../contexts/LanguageContext";
 
 export default function Header() {
   const [toggle, setToggle] = useState(false);
+  const [activeNav, setActiveNav] = useState("#home");
   const { language, changeLanguage, getTexts } = useLanguage();
   const texts = getTexts();
+
+  // --------- CHANGE ACTIVE LINK ----------
+  window.addEventListener("scroll", function () {
+    const header = document.querySelector(".header");
+    if (this.scrollY >= 80) header.classList.add("scroll-header");
+    else header.classList.remove("scroll-header");
+  });
 
   return (
     <div>
@@ -18,33 +26,73 @@ export default function Header() {
           <div className={toggle ? "nav__menu show-menu" : "nav__menu"}>
             <ul className="nav__list grid">
               <li className="nav__item">
-                <a href="#home" className="nav__link active-link">
+                <a
+                  href="#home"
+                  onClick={() => setActiveNav("#home")}
+                  className={
+                    activeNav === "#home"
+                      ? "nav__link active-link"
+                      : "nav__link"
+                  }
+                >
                   <i className="uil uil-estate nav__icon"></i> {texts.nav.home}
                 </a>
               </li>
 
               <li className="nav__item">
-                <a href="#about" className="nav__link">
+                <a
+                  href="#about"
+                  onClick={() => setActiveNav("#about")}
+                  className={
+                    activeNav === "#about"
+                      ? "nav__link active-link"
+                      : "nav__link"
+                  }
+                >
                   <i className="uil uil-user nav__icon"></i> {texts.nav.about}
                 </a>
               </li>
 
               <li className="nav__item">
-                <a href="#skills" className="nav__link">
+                <a
+                  href="#skills"
+                  onClick={() => setActiveNav("#skills")}
+                  className={
+                    activeNav === "#skills"
+                      ? "nav__link active-link"
+                      : "nav__link"
+                  }
+                >
                   <i className="uil uil-file-alt nav__icon"></i>{" "}
                   {texts.nav.skills}
                 </a>
               </li>
 
               <li className="nav__item">
-                <a href="#portfolio" className="nav__link">
+                <a
+                  href="#portfolio"
+                  onClick={() => setActiveNav("#portfolio")}
+                  className={
+                    activeNav === "#portfolio"
+                      ? "nav__link active-link"
+                      : "nav__link"
+                  }
+                >
                   <i className="uil uil-scenery nav__icon"></i>{" "}
                   {texts.nav.portfolio}
                 </a>
               </li>
 
               <li className="nav__item">
-                <a href="#contact" className="nav__link">
+                <a
+                  href="#contact"
+                  onClick={() => setActiveNav("#contact")}
+                  className={
+                    activeNav === "#contact"
+                      ? "nav__link active-link"
+                      : "nav__link"
+                  }
+                >
                   <i className="uil uil-message nav__icon"></i>{" "}
                   {texts.nav.contact}
                 </a>
@@ -57,7 +105,7 @@ export default function Header() {
                   style={{ cursor: "pointer" }}
                 >
                   <i className="uil uil-letter-japanese-a nav__icon"></i>
-                  {language === "en" ? "English" : "Türkçe"}
+                  {language === "en" ? "Language : English" : "Dil : Türkçe"}
                 </a>
               </li>
 
